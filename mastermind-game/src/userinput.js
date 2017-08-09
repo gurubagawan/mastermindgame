abf8f6403d5460367f3f52445998f9731199e933
BrainStation <brainstation@BrainStations-MacBook-Air.local>
2017-08-09 17:15:54 -0400

    made an array of sixteen buttons

--- mastermind-game/src/userinput.js
+++ mastermind-game/src/userinput.js
@@ -4,31 +4,59 @@ import './App.css';
 
 class Userinput extends Component {
   render() {
-    let board = this.props.board
+    let template = this.props.board
     //let changingclass = `btn btn-circle ${this.props.board[i].colour ? this.props.board[i].colour : ''}`;
     //use Push loop to create multiple rows for use input somehow. 
     //for (let z=0; z<8; z++) {
-var newBoard = []
-    var smallBoard =
-      board.map((specarr, i) => {
-        return (<button key={i} className={`btn btn-circle ${this.props.board[i].colour ? this.props.board[i].colour : ''}`} onClick={() => { this.props.setcolour((i + 1), this.props.colour) }} > </button>)
+//var newBoard = []
+    var newBoard =
+      template.map((singarr, i) => {
+        let x = singarr.map((button,j) => { 
+        return (<button key={j} className={`btn btn-circle ${this.props.board[i][j].colour ? this.props.board[i][j].colour : ''}`} onClick={() => { this.props.setcolour((i + 1), this.props.colour) }} > </button>)
+        })
+      return (<div key={i} > {x} </div>)
       })
 
 // var newBoard = 
 // smallBoard.map((specbtn, j)=> {
 //    return (smallBoard key={j})
 // })
-    for(let i=0; i<8; i++) {
-      newBoard.push(smallBoard)
-    }
-    //       var smallbrd = 
-    //       specarr.map ((obj, j) => {
-    //       return(
-    //       <button key={j} className= {`btn btn-circle ${this.props.board[i][j].colour ? this.props.board[i][j].colour : ''}`} onClick={() => { this.props.setcolour((i+1) , this.props.colour) }} > </button>
-    //  )
-    //  }) 
-    //}
-    console.log(smallBoard)
+//ar template = 
+//guess.map ((dot,i) => {
+
+// } )
+
+  //  var template =
+  //     board.map((guess, i) => {
+  //       "<div>"
+  //         guess.map((dot, j)=>{
+  //           return (<button key={i} className={`btn btn-circle ${this.props.board[i].colour ? this.props.board[i].colour : ''}`} onClick={() => { this.props.setcolour((i + 1), this.props.colour) }} > </button>)
+  //         }
+  //         "</div>"
+          
+        
+        
+  //     })
+
+// var newBoard = 
+// smallBoard.map((specbtn, j)=> {
+//    return (smallBoard key={j})
+// })
+// var template = 
+// guess.map ((dot,i) => {
+
+// } )
+//     for(let i=0; i<8; i++) {
+//       newBoard.push(smallBoard)
+//     }
+//     //       var smallbrd = 
+//     //       specarr.map ((obj, j) => {
+//     //       return(
+//     //       <button key={j} className= {`btn btn-circle ${this.props.board[i][j].colour ? this.props.board[i][j].colour : ''}`} onClick={() => { this.props.setcolour((i+1) , this.props.colour) }} > </button>
+//     //  )
+//     //  }) 
+//     //}
+//     console.log(smallBoard)
     return (
 
       //needs to update (remap) every input
